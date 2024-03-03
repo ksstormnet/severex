8e719b349626cd459adb3761c149ad6592991a19
ksstormnet <scott@ksstorm.net>
2024-03-03 14:21:22 -0600

    fix db test

--- tests/db.test.ts
+++ tests/db.test.ts
@@ -4,6 +4,11 @@ import Database from 'better-sqlite3';
 
 const dbName = './severex.db';
 
+interface TestRow {
+ id: number;
+ name: string;
+}
+
 describe('SQLite Database', () => {
 let db = new Database(dbName, { verbose: console.log });
 
@@ -15,7 +20,7 @@ beforeAll(() => {
 });
 
   it('should allow data insertion', () => {
-    // Attempt to create a table and insert data
+ // Attempt to create a table and insert data
     const createTable = db.prepare('CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)').run();
     expect(createTable).toBeDefined();
 
@@ -23,10 +28,10 @@ beforeAll(() => {
     expect(insert.lastInsertRowid).toBeDefined();
 
     // Verify data insertion
-    const row = db.prepare('SELECT * FROM test WHERE id = ?').get(insert.lastInsertRowid);
+    const row = db.prepare('SELECT * FROM test WHERE id = ?').get(insert.lastInsertRowid) as TestRow;
     expect(row).toBeDefined();
     expect(row.name).toBe('Test Name');
-  });
+ });
 
   afterAll(() => {
     // Clean up: delete test data and close the database
